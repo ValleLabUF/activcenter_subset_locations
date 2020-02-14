39bd3b160986d5f9e4e688b91d724b194643179c
Josh Cullen <joshcullen@Joshs-MacBook-Pro-3.local>
2020-02-14 08:49:24 -0500

    Create heatmap of monthly pattern of AC use

--- Step 4_Identify Activity Centers.R
+++ Step 4_Identify Activity Centers.R
@@ -98,8 +98,6 @@ for (i in 1:length(ac)) {
 }
 ac.ns<- unlist(ac.ns)  #ac assignments order N -> S
 
-# ac.coords<- read.csv("Activity Center Coordinates.csv", header = T, sep = ',')
-# ac<- read.csv("ac.csv", header = T, sep = ',')
 table(ac)
 table(ac.ns)
 
@@ -123,6 +121,9 @@ dat %>% group_by(id) %>% dplyr::select(ac) %>% table()
 
 
 ## Map
+
+# ac.coords<- read.csv("Activity Center Coordinates_TOHO.csv", header = T, sep = ',')
+# dat<- read.csv("Snail Kite Gridded Data_AC_TOHO.csv", header = T, sep = ',')
 
 #Load world map data
 usa<- ne_states(country = "United States of America", returnclass = "sf")
@@ -148,21 +149,52 @@ ggplot() +
   geom_point(data = nests, aes(x, y, fill = "Nests"), shape = 24, size = 3.5, alpha = 0.7) +
   geom_point(data = ac.coords, aes(x, y, fill = "ACs"), shape = 21, size = 3, alpha = 0.8) +
   labs(x="Longitude", y="Latitude") +
-  scale_fill_manual("", values = c(viridis(n=2)[1],"red","grey55")) +
+  scale_fill_manual("", values = c(viridis(n=5)[4],"red","grey55")) +
   guides(fill = guide_legend(override.aes = list(shape = c(21,24,21)))) +
   theme_bw()
 
 # ACs and snail kite locs
 ggplot() +
   geom_sf(data = fl) +
+  geom_sf(data = lakes10, fill = "lightblue", alpha = 0.65) +
   coord_sf(xlim = c(min(dat$x-20000), max(dat$x+20000)),
            ylim = c(min(dat$y-20000), max(dat$y+20000)), expand = FALSE) +
   geom_point(data = dat, aes(x, y, color=ac), size=1, alpha = 0.6) +
-  geom_point(data = ac.coords, aes(x, y, color = ac), size = 4, pch = 1, stroke = 1) +
+  geom_point(data = ac.coords, aes(x, y, color = ac.ns), size = 4, pch = 1, stroke = 1) +
   scale_color_viridis_c("Activity Center") +
   labs(x = "Longitude", y = "Latitude") +
   theme_bw() +
   facet_wrap(~id)
+
+
+## AC Heatmap by month and year (for SNIK 12)
+dat2<- dat[dat$id == "SNIK 12",]
+
+dat2<- dat2 %>% mutate(month = lubridate::month(date), year = lubridate::year(date))
+dat.sum<- dat2 %>% group_by(year, month, ac) %>% tally() %>% group_by(year, month) %>%
+  mutate(N=sum(n)) %>% mutate(prop = n/N)
+dat.sum$date<- as.Date(paste0(dat.sum$year,"-", dat.sum$month,"-01"), "%Y-%m-%d")
+
+foo<- matrix(0, nrow(ac.coords)*length(unique(dat.sum$date)), 3)
+colnames(foo)<- c("date","ac","prop")
+foo[,1]<- rep(unique(dat.sum$date), each=nrow(ac.coords))
+foo[,2]<- rep(1:nrow(ac.coords), length(unique(dat.sum$date)))
+for (i in 1:nrow(dat.sum)) {
+  ind<- which(dat.sum$date[i] == foo[,1] & dat.sum$ac[i] == foo[,2])
+  foo[ind,3]<- dat.sum$prop[i]
+}
+foo<- data.frame(foo)
+
+ggplot(data = foo, aes(x=lubridate::as_date(date), y=ac)) +
+  geom_tile(aes(fill=prop), width = 31) +
+  scale_fill_viridis_c("Proportion of\nObservations\nper Month") +
+  scale_y_continuous(trans = "reverse", breaks = 1:20, expand = c(0,0)) +
+  scale_x_date(date_labels = "%b %Y", expand = c(0,0)) +
+  labs(x="Time", y="Activity Center") +
+  theme_bw() +
+  theme(panel.grid = element_blank(), axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12))
+
 
 
 ###################
